862eccea85bce51883172c5891c34aa709ce4d49
Pritz Belleza <pritz.belleza@gmail.com>
2023-12-04 00:35:17 +0800

    adds header nav and banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import heroBanner from './assets/hero-banner-desktop.jpeg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navHeaders = [
+    {
+      name: 'Home',
+      url: '/',
+    },
+    {
+      name: 'Recipes',
+      url: '/recipes',
+    },
+    {
+      name: 'About',
+      url: '/about',
+    },
+    {
+      name: 'Contact us',
+      url: '/contact-us',
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header>
+        {/* hero banner */}
+        <div className="Hero">
+          <img className="Hero--img" src={heroBanner} alt="just cook banner" />
+        </div>
+        {/* nav */}
+        <div className="Nav">
+          <nav>
+            <ul className="Nav--ul">
+              {navHeaders.map((navHeader) => (
+                <li key={navHeader.name}>{navHeader.name}</li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {/* content */}
+      <main>
+        <div></div>
+      </main>
+      {/* footer */}
+      <footer>
+        <div></div>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
