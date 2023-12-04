c616f0112abeb496925557ec3c04b8e2a10bae1c
Pritz Belleza <pritz.belleza@gmail.com>
2023-12-04 22:23:05 +0800

    add recipes content

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 import heroBanner from './assets/hero-banner-desktop.jpeg';
 
+const navHeaders = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'Recipes',
+    url: '/recipes',
+  },
+  {
+    name: 'About',
+    url: '/about',
+  },
+  {
+    name: 'Contact us',
+    url: '/contact-us',
+  },
+];
+
 function App() {
-  const navHeaders = [
-    {
-      name: 'Home',
-      url: '/',
-    },
-    {
-      name: 'Recipes',
-      url: '/recipes',
-    },
-    {
-      name: 'About',
-      url: '/about',
-    },
-    {
-      name: 'Contact us',
-      url: '/contact-us',
-    },
-  ];
+  const [foodRecipes, setFoodRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const getRecipes = async () => {
+    setIsLoading(true);
+    try {
+      const data = await fetch('http://localhost:3001/recipes');
+      const recipes = await data.json();
+      setFoodRecipes(recipes);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   return (
     <div>
@@ -41,11 +61,30 @@ function App() {
       </header>
       {/* content */}
       <main>
-        <div></div>
+        <div className="Content">
+          {isLoading ? (
+            <span>Loading...</span>
+          ) : (
+            <div className="Recipes">
+              {foodRecipes.map((food) => (
+                <div className="RecipeCard" key={food.uuid}>
+                  <img src={`src/assets/${food.images.medium}`} />
+                  <div className="RecipeCard__description">
+                    <h3>{food.title}</h3>
+                    <p>{food.description}</p>
+                    <button>View recipe</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </main>
       {/* footer */}
       <footer>
-        <div></div>
+        <div className="Footer">
+          <p>Just Cook | The Best Recipe &copy; and &#169; 2012 - 2022</p>
+        </div>
       </footer>
     </div>
   );
